e9e466fd6ee72108b5637f055dfb04baf5598b9f
Kakon Ray <thisiskakonray@gmail.com>
2022-02-14 01:28:17 +0600

    Completed

--- script.js
+++ script.js
@@ -3,6 +3,26 @@ let input = document.getElementById("input");
 let random = document.getElementById("random");
 let pinGen = document.getElementById("pinGen");
 
+function check(pinmatch, dontMatch) {
+  document.getElementById("pinmatch").style.display = pinmatch;
+  document.getElementById("dontMatch").style.display = dontMatch;
+  input.value = "";
+}
+
+function verifyInputValue() {
+  if (input.value === random.value) {
+    check("block", "none");
+  } else {
+    check("none", "block");
+  }
+}
+
+function errorShow() {
+  if (clicks >= 0) document.getElementById("leftItem").innerText = clicks;
+  else
+    document.getElementById("leftItem").innerText = "Please Waite 10 Minutes";
+}
+
 pinGen.addEventListener("click", () => {
   random.value = Math.floor(Math.random() * 10000) + 1;
 });
@@ -10,22 +30,22 @@ pinGen.addEventListener("click", () => {
 document.getElementById("keypad").addEventListener("click", (e) => {
   input.value = input.value + e.target.innerText;
 });
+
 let clicks = 3;
 submit.addEventListener("click", (e) => {
-  console.log(random.value);
-  console.log(input.value);
-  if (input.value === random.value) {
-    document.getElementById("pinmatch").style.display = "block";
-    document.getElementById("dontMatch").style.display = "none";
+  if (input.value === "") {
+    document.getElementById("leftItem").innerText =
+      "Please add Number Input Fild";
+    if (e.target.innerText === "Submit") {
+      e.stopPropagation();
+    }
   } else {
-    document.getElementById("pinmatch").style.display = "none";
-    document.getElementById("dontMatch").style.display = "block";
+    clicks -= 1;
+    verifyInputValue();
+    if (e.target.innerText === "Submit") {
+      e.stopPropagation();
+    }
+
+    errorShow();
   }
-  if (e.target.innerText === "Submit") {
-    e.stopPropagation();
-  }
-  clicks -= 1;
-  if (clicks >= 0) document.getElementById("leftItem").innerText = clicks;
-  else
-    document.getElementById("leftItem").innerText = "Please Waite 10 Minutes";
 });
